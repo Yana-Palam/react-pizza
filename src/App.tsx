f1a6ff1777cbf02a2288ea790e59d0c649690f11
unknown <tana.palam@gmail.com>
2023-07-07 14:11:11 +0300

    fix path

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import {
   Navigate,
   RouterProvider,
 } from "react-router-dom";
-import React, { lazy, Suspense } from "react";
+import React, { Suspense } from "react";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 import Layout from "./components/Layout/Layout";
 import Loader from "./components/Loader/Loader";
-const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const CartPage = lazy(() => import("./pages/CartPage/CartPage"));
+import HomePage from "./pages/HomePage/HomePage";
+import CartPage from "./pages/CartPage/CartPage";
 
 const App: React.FC = () => {
   const router = createBrowserRouter(
